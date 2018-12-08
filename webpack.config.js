7bc9b6de8555b7d60ca86d8803f8e48f0f273d9e
Sia Karamalegos <siakaramalegos@gmail.com>
2018-12-08 00:27:11 +0000

    Update dependencies for security

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 const staticPath = path.resolve(__dirname, 'app/static/');
 const environment = process.env.NODE_ENV || 'development'
@@ -26,16 +26,14 @@ const commonConfig = merge([{
     rules: [
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            { loader: 'css-loader', options: { sourceMap: true } },
-            // config in postcss.config.js and browserslist in package.json
-            { loader: 'postcss-loader', options: { sourceMap: true } },
-            { loader: 'resolve-url-loader', options: { keepQuery: true } },
-            { loader: 'sass-loader', options: { sourceMap: true } },
-          ],
-        })
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { sourceMap: true } },
+          // config in postcss.config.js and browserslist in package.json
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+          { loader: 'resolve-url-loader', options: { keepQuery: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
@@ -84,7 +82,12 @@ const commonConfig = merge([{
     ]
   },
   plugins: [
-    new ExtractTextPlugin('style.css'),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "style.css",
+      chunkFilename: "[id].css"
+    }),
     new CleanWebpackPlugin(['build']),
     // This makes it possible for us to safely use env vars on our code
     new webpack.DefinePlugin({
