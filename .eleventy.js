ddb1a9c926e6e195f9afeeb5b9448f19e0b48716
Sia Karamalegos <siakaramalegos@gmail.com>
2021-06-07 12:45:02 -0500

    Convert to eleventy

---
+++ .eleventy.js
@@ -0,0 +1,48 @@
+// const filters = require('./src/_11ty/filters');
+
+module.exports = function(eleventyConfig) {
+  // Filters
+  // Object.keys(filters).forEach(filterName => {
+  //   eleventyConfig.addFilter(filterName, filters[filterName])
+  // })
+
+  // // Shortcodes
+  // Object.keys(shortcodes).forEach(shortcodeName => {
+  //   eleventyConfig.addShortcode(shortcodeName, shortcodes[shortcodeName])
+  // })
+
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/style.css");
+  eleventyConfig.addPassthroughCopy("src/fonts/");
+  eleventyConfig.addPassthroughCopy("src/images/");
+  eleventyConfig.addPassthroughCopy("src/javascripts/");
+  eleventyConfig.addPassthroughCopy("src/static/");
+
+
+  // Input directory: src
+  // Output directory: _site
+  return {
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      // "liquid"
+    ],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/",
+
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+};
